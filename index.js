465d7a6e40cfbf732e606a67e0f4734f2ffff533
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-03-22 22:30:52 +0530

    update

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 class Book {
   constructor() {
-    this.books = JSON.parse(localStorage.getItem("books")) || [];
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
     this.addBook = this.addBook.bind(this);
     this.removeBook = this.removeBook.bind(this);
   }
 
   addBook() {
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
     if (title && author) {
       this.books.push({ title, author });
-      localStorage.setItem("books", JSON.stringify(this.books));
+      localStorage.setItem('books', JSON.stringify(this.books));
       this.displayBooks();
-      document.getElementById("title").value = "";
-      document.getElementById("author").value = "";
+      document.getElementById('title').value = '';
+      document.getElementById('author').value = '';
     }
   }
 
   removeBook(index) {
     this.books.splice(index, 1);
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
     this.displayBooks();
   }
 
   displayBooks() {
-    const bookList = document.getElementById("books");
-    bookList.innerHTML = "";
+    const bookList = document.getElementById('books');
+    bookList.innerHTML = '';
     this.books.forEach((book, index) => {
-      const li = document.createElement("li");
-      const h3 = document.createElement("h3");
-      const removeBtn = document.createElement("button");
+      const li = document.createElement('li');
+      const h3 = document.createElement('h3');
+      const removeBtn = document.createElement('button');
       h3.textContent = `${book.title} by ${book.author}`;
-      removeBtn.textContent = "Remove";
-      removeBtn.addEventListener("click", () => this.removeBook(index));
-      li.classList.add("book-item");
+      removeBtn.textContent = 'Remove';
+      removeBtn.addEventListener('click', () => this.removeBook(index));
+      li.classList.add('book-item');
       li.appendChild(h3);
       li.appendChild(removeBtn);
       bookList.appendChild(li);
@@ -42,22 +42,20 @@ class Book {
 }
 
 const bookCollect = new Book();
-document
-  .getElementById("add-btn")
-  .addEventListener("click", bookCollect.addBook);
+document.getElementById('add-btn').addEventListener('click', bookCollect.addBook);
 bookCollect.displayBooks();
 
-const links = document.querySelectorAll("nav a");
-const secTion = document.querySelectorAll("section");
+const links = document.querySelectorAll('nav a');
+const secTion = document.querySelectorAll('section');
 
 links.forEach((link) => {
-  link.addEventListener("click", () => {
-    links.forEach((link) => link.classList.remove("active"));
-    link.classList.add("active");
-    secTion.forEach((section) => (section.style.display = "none"));
+  link.addEventListener('click', () => {
+    links.forEach((link) => link.classList.remove('active'));
+    link.classList.add('active');
+    secTion.forEach((section) => section.style.display = 'none');
 
-    const sectionId = link.getAttribute("href").slice(1);
-    document.getElementById(sectionId).style.display = "block";
+    const sectionId = link.getAttribute('href').slice(1);
+    document.getElementById(sectionId).style.display = 'block';
   });
 });
 
