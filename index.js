03e99cd6eab2f9986c65031cd64065b61f2ca8c5
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-03-22 15:22:09 +0530

    update-css

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ class Book {
     this.removeBook = this.removeBook.bind(this);
   }
 
-  addBook () {
+  addBook() {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     if (title && author) {
-      this.books.push({title, author});
+      this.books.push({ title, author });
       localStorage.setItem('books', JSON.stringify(this.books));
       this.displayBooks();
       document.getElementById('title').value = '';
@@ -31,7 +31,7 @@ class Book {
       const h3 = document.createElement('h3');
       const removeBtn = document.createElement('button');
       h3.textContent = `${book.title} by ${book.author}`;
-      removeBtn.textContent = `Remove`;
+      removeBtn.textContent = 'Remove';
       removeBtn.addEventListener('click', () => this.removeBook(index));
       li.classList.add('book-item');
       li.appendChild(h3);
